d95cebbbac9f471865e44267c835f7c4a9662d94
AirikU <a.usami.sys21@morijyobi.ac.jp>
2023-09-08 14:01:06 +0900

    [Update]ボタン機能実装 [issue #14] - ref_game_main.html         - 先ほど描画したものをボタン化 - ref_game_main.css         - 各ボタンのデザイン - ref_game.js         - 各ボタンがクリックされたときの処理をコンソールに表示(確認用) 	- ※DOMを使用

--- static/js/ref_game.js
+++ static/js/ref_game.js
@@ -1,23 +1,37 @@
-function draw() {
-    var canvas = document.getElementById('square');
-    var x = 50;
-    var y = 50;
-    var wid = 100;
-    var hei = 100;
-    var count = 0;
-    var ctx = canvas.getContext('2d');
-    while( count < 9 ) {
-        ctx.fillStyle = 'rgb(255, 255, 255)';
-        ctx.fillRect(x, y, wid, hei);
-        ctx.strokeStyle = 'rgb(0, 0, 0)';
-        ctx.strokeRect(x, y, wid, hei);
-        count++;
-        console.log(count);
-        if( count % 3 == 0 ) {
-            x+=150;
-            y = 50;
-        } else {
-            y+=150;
-        }
-    }
-}
+'use strict';
+
+document.getElementById('button1').addEventListener('click', function(){
+    console.log('id名「' + this.id + '」のボタンを押しました。');
+});
+
+document.getElementById('button2').addEventListener('click', function(){
+    console.log('id名「' + this.id + '」のボタンを押しました。');
+});
+
+document.getElementById('button3').addEventListener('click', function(){
+    console.log('id名「' + this.id + '」のボタンを押しました。');
+});
+
+document.getElementById('button4').addEventListener('click', function(){
+    console.log('id名「' + this.id + '」のボタンを押しました。');
+});
+
+document.getElementById('button5').addEventListener('click', function(){
+    console.log('id名「' + this.id + '」のボタンを押しました。');
+});
+
+document.getElementById('button6').addEventListener('click', function(){
+    console.log('id名「' + this.id + '」のボタンを押しました。');
+});
+
+document.getElementById('button7').addEventListener('click', function(){
+    console.log('id名「' + this.id + '」のボタンを押しました。');
+});
+
+document.getElementById('button8').addEventListener('click', function(){
+    console.log('id名「' + this.id + '」のボタンを押しました。');
+});
+
+document.getElementById('button9').addEventListener('click', function(){
+    console.log('id名「' + this.id + '」のボタンを押しました。');
+});
