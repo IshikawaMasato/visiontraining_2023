428cfd25ca75b728fffd5b31df89855389442b68
AirikU <a.usami.sys21@morijyobi.ac.jp>
2023-09-09 02:21:10 +0900

    [Add]タイマーの数字部分追加＆jsファイル名変更＆読込み [issue #14] - ref_game_main.html 	- タイマーのjsファイル(ref_game_timer.js)読込み - ref_game_button.js 	- ref_game.jsから名称変更 	- ref_game_main.html で読込み - ref_game_timer.js 	- タイマーのカウントダウン処理 	- 残り時間0秒のときの処理(今後のコミットで再度更新予定)

---
+++ static/js/ref_game_timer.js
@@ -0,0 +1,35 @@
+var limitTime = 5;
+var startTime = Date.now();
+var timeDiff;
+var score;
+
+var countdown = function () {
+    timeDiff = Date.now() - startTime;
+    timeDiff = limitTime - (timeDiff / 1000);
+    timeDiff*=10;
+    timeDiff = Math.floor(timeDiff);
+    timeDiff = timeDiff / 10;
+    if( (timeDiff * 10) % 10 == 0 ) {
+        document.getElementById('timer').innerText = timeDiff + ".0秒";
+    } else {
+        document.getElementById('timer').innerText = timeDiff + "秒";
+    }
+    
+}
+
+var id = setInterval(function () {
+    countdown();
+    if(timeDiff <= 0) {
+        clearInterval(id);
+        document.getElementById('timer').innerText = "0.0秒";
+        document.getElementById('main').hide;
+        // $('.main').hide();
+    }
+}, 1);
+// var timer = document.getElementById("timer");
+// function count_down() {
+//     count--;
+
+// }
+
+var jikou = setInterval(count, 5000);
