e9021a7e064c0ef120b2927dc3e4ab8c88e07f50
AirikU <a.usami.sys21@morijyobi.ac.jp>
2023-09-12 16:16:53 +0900

    [Update]反射神経ゲームの結合処理 [issue #64] - 各htmlファイル 	- aタグのhrefを {{ url_for('ref_game.ref_game')}} に変更 - app.py 	- ref_game_bpをインポート 	- app.register_blueprint(ref_game_bp)を追加 - ref_game.py作成 	- dva_game.pyを参考に作成した

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, session
 import db,string,random
 from user import user_bp
 from dva_game import dva_game_bp
+from ref_game import ref_game_bp
 from vision_register import vision_bp
 from datetime import time
 
@@ -10,6 +11,7 @@ app.secret_key = ''.join(random.choices(string.ascii_letters, k=256))
 
 app.register_blueprint(user_bp)
 app.register_blueprint(dva_game_bp)
+app.register_blueprint(ref_game_bp)
 app.register_blueprint(vision_bp)
 # app.permanent_session_lifetime = timedelta(minutes=3)
 
