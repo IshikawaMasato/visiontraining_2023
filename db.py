d3ec34bd2a0dd4aefdd4d5730cdb101ad34f3b49
allface017 <113155656+allface017@users.noreply.github.com>
2023-09-20 10:38:23 +0900

    videoの部分を修正

--- db.py
+++ db.py
@@ -2,7 +2,7 @@ from flask import render_template
 import os, psycopg2, string, random, hashlib
 
 def get_connection():
-    url = os.environ['DATABASE_URL1']
+    url = os.environ['DATABASE_URL']
     connection = psycopg2.connect(url)
     return connection
 
